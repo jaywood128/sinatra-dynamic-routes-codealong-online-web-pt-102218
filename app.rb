1167dda05652a974de00fdca33650b9bf4fa7084
Johnathon Wood <jaywood128@gmail.com>
2019-02-06 16:13:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
   end# Code your final two routes here:
 
   get "/multiply/:num1/:num2" do
-  @number1 = params[:num1]
+  @number1 = params[:num1].to_i
   @number2 = params[:num2]
   binding.pry
   end
